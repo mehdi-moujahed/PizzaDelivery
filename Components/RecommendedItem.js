e4dc8c3305548f00c244e4a2f5cb8d5fc602296c
Mehdi <moujahedmehdi@gmail.com>
2020-09-01 17:13:29 +0100

    Recommended Items screen

---
+++ Components/RecommendedItem.js
@@ -0,0 +1,329 @@
+import React from 'react';
+import {View, StyleSheet, Image, Text} from 'react-native';
+import {TouchableOpacity, ScrollView} from 'react-native-gesture-handler';
+import Icons from 'react-native-vector-icons/FontAwesome';
+
+export default class RecommendedItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <ScrollView style={{flex: 1}}>
+        <View style={styles.main_container}>
+          <View style={styles.pizza_container}>
+            <View style={{alignItems: 'center'}}>
+              <Image
+                source={require('../Images/pizza11.jpeg')}
+                style={styles.img_pizza}
+              />
+            </View>
+            <View
+              style={{
+                marginLeft: 20,
+                marginTop: 15,
+              }}>
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontFamily: 'SFProDisplay-Medium',
+                  color: '#3B3B3B',
+                }}>
+                Pizza Margherita
+              </Text>
+              <View style={{flexDirection: 'row', marginTop: 10}}>
+                <Icons name="star" size={18} style={{color: '#F34949'}} />
+                <Icons name="star" size={18} style={{color: '#F34949'}} />
+                <Icons name="star" size={18} style={{color: '#F34949'}} />
+                <Icons name="star" size={18} style={{color: '#F34949'}} />
+                <Icons name="star" size={18} style={{color: '#A9A9B0'}} />
+              </View>
+              <View style={styles.pizza_size}>
+                <TouchableOpacity
+                  style={{
+                    borderColor: '#F34949',
+                    borderWidth: 1.5,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}
+                  onPress={() => console.log('Small pizza selected !')}>
+                  <Text
+                    style={{
+                      color: '#F34949',
+                      fontSize: 13,
+                      fontFamily: 'SFProDisplay-Medium',
+                      margin: 4,
+                    }}>
+                    Small{' '}
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: 13,
+                      fontFamily: 'SFProDisplay-Medium',
+                      margin: 4,
+                      color: '#F34949',
+                    }}>
+                    8DT
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => console.log('medium pizza selected !')}
+                  style={{
+                    borderColor: '#A9A9B0',
+                    borderWidth: 1.5,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}>
+                  <Text
+                    style={{
+                      color: '#A9A9B0',
+                      fontSize: 13,
+                      fontFamily: 'SFProDisplay-Medium',
+                      margin: 4,
+                    }}>
+                    Medium{' '}
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: 13,
+                      fontFamily: 'SFProDisplay-Medium',
+                      margin: 4,
+                      color: '#A9A9B0',
+                    }}>
+                    12DT
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => console.log('large pizza selected !')}
+                  style={{
+                    borderColor: '#A9A9B0',
+                    borderWidth: 1.5,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    marginRight: 15,
+                  }}>
+                  <Text
+                    style={{
+                      color: '#A9A9B0',
+                      fontSize: 13,
+                      fontFamily: 'SFProDisplay-Medium',
+                      margin: 4,
+                    }}>
+                    Large{' '}
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: 13,
+                      fontFamily: 'SFProDisplay-Medium',
+                      margin: 4,
+                      color: '#A9A9B0',
+                    }}>
+                    15DT
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.button_container}>
+              <TouchableOpacity
+                style={styles.customize_button}
+                onPress={() => this.props.navigation.navigate('Details')}>
+                <Text style={styles.customizeButtonText}>Customize & Add</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.addCart_button}
+                onPress={() => console.log('Add to Cart Pressed !')}>
+                <Text style={styles.addCartButtonText}>Add to Cart</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <View style={styles.pizza_container}>
+            <View style={{alignItems: 'center'}}>
+              <Image
+                source={require('../Images/pizza11.jpg')}
+                style={styles.img_pizza}
+              />
+            </View>
+            <View
+              style={{
+                marginLeft: 20,
+                marginTop: 15,
+              }}>
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontFamily: 'SFProDisplay-Medium',
+                  color: '#3B3B3B',
+                }}>
+                Veggie Cheese Extravagenza
+              </Text>
+              <View style={{flexDirection: 'row', marginTop: 10}}>
+                <Icons name="star" size={18} style={{color: '#F34949'}} />
+                <Icons name="star" size={18} style={{color: '#F34949'}} />
+                <Icons name="star" size={18} style={{color: '#F34949'}} />
+                <Icons name="star" size={18} style={{color: '#F34949'}} />
+                <Icons name="star" size={18} style={{color: '#F34949'}} />
+              </View>
+              <View style={styles.pizza_size}>
+                <TouchableOpacity
+                  style={{
+                    borderColor: '#F34949',
+                    borderWidth: 1.5,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}
+                  onPress={() => console.log('Small pizza selected !')}>
+                  <Text
+                    style={{
+                      color: '#F34949',
+                      fontSize: 13,
+                      fontFamily: 'SFProDisplay-Medium',
+                      margin: 4,
+                    }}>
+                    Small{' '}
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: 13,
+                      fontFamily: 'SFProDisplay-Medium',
+                      margin: 4,
+                      color: '#F34949',
+                    }}>
+                    10DT
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => console.log('medium pizza selected !')}
+                  style={{
+                    borderColor: '#A9A9B0',
+                    borderWidth: 1.5,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}>
+                  <Text
+                    style={{
+                      color: '#A9A9B0',
+                      fontSize: 13,
+                      fontFamily: 'SFProDisplay-Medium',
+                      margin: 4,
+                    }}>
+                    Medium{' '}
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: 13,
+                      fontFamily: 'SFProDisplay-Medium',
+                      margin: 4,
+                      color: '#A9A9B0',
+                    }}>
+                    15DT
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => console.log('large pizza selected !')}
+                  style={{
+                    borderColor: '#A9A9B0',
+                    borderWidth: 1.5,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    marginRight: 15,
+                  }}>
+                  <Text
+                    style={{
+                      color: '#A9A9B0',
+                      fontSize: 13,
+                      fontFamily: 'SFProDisplay-Medium',
+                      margin: 4,
+                    }}>
+                    Large{' '}
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: 13,
+                      fontFamily: 'SFProDisplay-Medium',
+                      margin: 4,
+                      color: '#A9A9B0',
+                    }}>
+                    20DT
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.button_container}>
+              <TouchableOpacity
+                style={styles.customize_button}
+                onPress={() => this.props.navigation.navigate('Details')}>
+                <Text style={styles.customizeButtonText}>Customize & Add</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.addCart_button}
+                onPress={() => console.log('Add to Cart Pressed !')}>
+                <Text style={styles.addCartButtonText}>Add to Cart</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  pizza_container: {
+    marginTop: 30,
+    marginLeft: 15,
+    marginRight: 15,
+    borderRadius: 8,
+    height: 390,
+    width: 335,
+    elevation: 10,
+    backgroundColor: '#FBFBFB',
+  },
+  main_container: {
+    flex: 1,
+  },
+  img_pizza: {
+    width: 295,
+    height: 172,
+    borderRadius: 13,
+    marginTop: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pizza_size: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  button_container: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  customize_button: {
+    backgroundColor: '#e8e8e8',
+    width: 295,
+    height: 30,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  addCart_button: {
+    backgroundColor: '#F34949',
+    width: 295,
+    height: 30,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  customizeButtonText: {
+    fontSize: 12,
+    color: '#3B3B3B',
+    fontFamily: 'SFProDisplay-Semibold',
+  },
+  addCartButtonText: {
+    fontSize: 12,
+    color: '#fff',
+    fontFamily: 'SFProDisplay-Semibold',
+  },
+});
--- Screens/Recommended.js
+++ Screens/Recommended.js
@@ -1,13 +1,19 @@
 import * as React from 'react';
-import {View, StyleSheet, Dimensions} from 'react-native';
+import {View, StyleSheet, Dimensions, Text} from 'react-native';
 import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
+import Icons from 'react-native-vector-icons/Ionicons';
+import RecommendedItem from '../Components/RecommendedItem';
 
 const FirstRoute = () => (
-  <View style={[styles.scene, {backgroundColor: 'black'}]} />
+  <View style={[styles.scene, {backgroundColor: 'white'}]}>
+    <RecommendedItem></RecommendedItem>
+  </View>
 );
 
 const SecondRoute = () => (
-  <View style={[styles.scene, {backgroundColor: '#673ab7'}]} />
+  <View style={[styles.scene, {backgroundColor: 'white'}]}>
+    <RecommendedItem></RecommendedItem>
+  </View>
 );
 
 const ThirdRoute = () => (
@@ -21,7 +27,7 @@ export default function Recommended() {
   const [routes] = React.useState([
     {key: 'first', title: 'Five Stars'},
     {key: 'second', title: 'Four Stars'},
-    {key: 'third', title: 'Three Stars'},
+    {key: 'third', title: 'Comments'},
   ]);
 
   const renderScene = SceneMap({
@@ -32,14 +38,29 @@ export default function Recommended() {
   const renderTabBar = (props) => (
     <TabBar
       {...props}
-      indicatorStyle={{backgroundColor: 'white'}}
-      style={{backgroundColor: '#F34949', elevation: 0}}
-      activeColor={'white'}
-      inactiveColor="black"
+      indicatorStyle={{backgroundColor: '#F34949'}}
+      style={{backgroundColor: 'white', elevation: 0}}
+      activeColor={'#F34949'}
+      inactiveColor="grey"
     />
   );
   return (
     <View style={styles.main_container}>
+      <View style={styles.top_container}>
+        <View style={{flex: 0.2}}>
+          <Icons
+            name="ios-arrow-back-sharp"
+            size={25}
+            onPress={() => this.props.navigation.goBack()}
+          />
+        </View>
+        <View
+          style={{justifyContent: 'center', alignItems: 'center', flex: 0.6}}>
+          <Text style={{fontSize: 20, fontFamily: 'SFProDisplay-Regular'}}>
+            Recommended
+          </Text>
+        </View>
+      </View>
       <TabView
         navigationState={{index, routes}}
         renderScene={renderScene}
@@ -62,6 +83,12 @@ const styles = StyleSheet.create({
   },
   tabview: {
     flex: 1,
-    marginTop: 120,
+  },
+  top_container: {
+    flexDirection: 'row',
+    marginTop: 40,
+    marginHorizontal: 20,
+    marginBottom: 30,
+    alignItems: 'center',
   },
 });
